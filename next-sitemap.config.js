4f238f7d875e7d100807b22e2b842ebce76a9916
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-08-14 16:38:18 +0530

    add site-map

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://0xticket.vercel.app/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+
+  module.exports = config;
