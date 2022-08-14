ab7521b7f8c7012fac58fb6333c19e05e29b3112
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-08-14 17:37:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const webpack = require('webpack')
 
 module.exports = {
+  compiler: {
+    removeConsole: true,
+  },
   images: {
     domains: ["images.unsplash.com", "source.unsplash.com", "ipfs.io", "firebasestorage.googleapis.com", "ipfs.infura.io","localhost"],
   },
